707b7ab5fc42f4e9778affdd9167abef06e52d45
mrschulte <mr.schulte@outlook.com>
2021-02-25 21:19:56 +0100

    Designed Menu

--- Project_TicTacToe/Client/Form_Menu.cs
+++ Project_TicTacToe/Client/Form_Menu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Client
 {
-    public partial class Form1 : Form
+    public partial class Form_Menu : Form
     {
-        public Form1()
+        public Form_Menu()
         {
             InitializeComponent();
         }
